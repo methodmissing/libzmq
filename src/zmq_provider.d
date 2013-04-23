2ca5ae12464a35dc4e0250599030e0a190204779
Lourens Naudé <lourens@methodmissing.com>
2013-04-23 22:51:21 +0100

    Initial dtrace build system integration

---
+++ src/zmq_provider.d
@@ -0,0 +1,8 @@
+provider zmq {
+};
+
+#pragma D attributes Stable/Stable/Common	provider zmq provider
+#pragma D attributes Stable/Stable/Common	provider zmq module
+#pragma D attributes Stable/Stable/Common	provider zmq function
+#pragma D attributes Stable/Stable/Common	provider zmq name
+#pragma D attributes Stable/Stable/Common	provider zmq args
